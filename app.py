94be4c61efd4e199e76e94e76ca4d158c1e92aa6
bharatreddy2019 <55209591+bharatreddy2019@users.noreply.github.com>
2019-10-01 10:44:53 -0500

    Update app.py
    Added new variables

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ def index():
     text = "{}<br><br>Here's my secret: {}".format(
         os.environ.get('APP_MESSAGE', 'Hello, World!'),
         os.environ.get('APP_SECRET')
+        os.environ.get('SFDC_TEST_USERNAME')
+        os.environ.get('SFDC_TEST_PASSWORD')
+        os.environ.get('SFDC_TEST_TOKEN')
+        os.environ.get('KEYID')
+        os.environ.get('SKEYID')
+        os.environ.get('BUCKETNAME')
+        os.environ.get('RUNMODE')
+        os.environ.get('INCREMENTALMODE')
     )
     return text
 
