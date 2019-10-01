e60f409ac58a5e5e82c1a4858c03b325296bfbc2
bharatreddy2019 <55209591+bharatreddy2019@users.noreply.github.com>
2019-10-01 10:57:19 -0500

    Update app.py
    Fixed comma error

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 def index():
     text = "{}<br><br>Here's my secret: {}".format(
         os.environ.get('APP_MESSAGE', 'Hello, World!'),
-        os.environ.get('APP_SECRET')
-        os.environ.get('SFDC_TEST_USERNAME')
-        os.environ.get('SFDC_TEST_PASSWORD')
-        os.environ.get('SFDC_TEST_TOKEN')
-        os.environ.get('KEYID')
-        os.environ.get('SKEYID')
-        os.environ.get('BUCKETNAME')
-        os.environ.get('RUNMODE')
+        os.environ.get('APP_SECRET'),
+        os.environ.get('SFDC_TEST_USERNAME'),
+        os.environ.get('SFDC_TEST_PASSWORD'),
+        os.environ.get('SFDC_TEST_TOKEN'),
+        os.environ.get('KEYID'),
+        os.environ.get('SKEYID'),
+        os.environ.get('BUCKETNAME'),
+        os.environ.get('RUNMODE'),
         os.environ.get('INCREMENTALMODE')
     )
     return text
